e6172b39996d0c620c8d984b8f2366e11dd63bf0
Нурлан <@->
2022-10-28 21:50:45 +0400

    Добавьте файлы проекта.

---
+++ umn6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace umn6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Double A, B,C;
+            C = 0;
+            B = 0;
+            A = 0;
+            try
+            {
+                Console.WriteLine("Введите первое число:");
+                A = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите воторое число:");
+                B = Convert.ToDouble(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Введите число:");
+                Console.ReadKey();
+            }
+            for (int i=0;  i<B; i++ )
+            {
+              C += A;
+
+            }
+            Console.WriteLine(C);
+            Console.ReadKey();
+        }
+    }
+}
